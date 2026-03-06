e679c678c8ffd62fc54ccf371f03c3d15a322afb
Aurora <243707@ids.upchiapas.edu.mx>
2026-03-06 01:40:11 -0600

    correción

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
       {
         source: "/api/:path*",
         headers: [
-          { key: "Access-Control-Allow-Origin", value: "https://ghibli-frontend-five.vercel.app" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
           { key: "Access-Control-Allow-Methods", value: "GET, OPTIONS" },
           { key: "Access-Control-Allow-Headers", value: "Content-Type" },
         ],
